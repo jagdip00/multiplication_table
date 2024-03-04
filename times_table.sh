5ecbec9cb09145cb95493d378617a656801692ed
jagdip00 <sjagdip18@gmail.com>
2024-03-04 04:24:50 +0000

    added validation for first question

--- times_table.sh
+++ times_table.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+while true
+do
 
 read -p "Enter a number for the multiplication table: " num
 
+if [ $num -lt 0 ]; then
+
+continue
+
+else
+
 read -p "Do you want a full table or a partial table? (Enter 'f' for full, 'p' for partial): " ans
+
+fi
+
 
 if [ $ans = "f" ]; then
 
 echo "The full multiplication table for $num: "
+
 
 for i in {1..10}
 do
     total=$(($num * $i))
     echo "$num x $i = $total"
 done
+
+
+
 
 elif [ $ans = "p" ]; then
 
@@ -30,21 +45,15 @@ done
 
 fi
 
+read -p "Do you want to use the program again? (Enter 'y' for yes, 'n' for no): " ans1
 
-#Enter the starting number (between 1 and 10): 2
+if [ $ans1 != "y" | $ans1 != "n" ]; then
 
-#Enter the ending number (between 1 and 10): 4
+continue
 
-#The partial multiplication table for 3 from 2 to 4:
-#3 x 2 = 6
-#3 x 3 = 9
-#3 x 4 = 12
+else
+echo "Thank you for using the program"
+break
+fi
 
-
-#if [ $num -lt 0 ]; then
-#    echo "Please enter a positive number."
-
-#else
-
-
-#fi
+done
