22b14e7e43e9edd073053e88b399e6b6d566a0f2
jagdip00 <sjagdip18@gmail.com>
2024-03-05 00:40:23 +0000

    added validation to partial multiplication option

--- times_table.sh
+++ times_table.sh
@@ -2,6 +2,7 @@
 while true
 do
 
+#######################################################################################################
 read -p "Enter a number for the multiplication table: " num
 
 if [ $num -lt 0 ]; then
@@ -31,9 +32,21 @@ done
 
 elif [ $ans = "p" ]; then
 
+while true
+do
+
 read -p "Enter the starting number (between 1 and 10): " num1
 
 read -p "Enter the ending number (between 1 and 10): " num2
+
+if [ $num1 -gt $num2 ]; then
+continue
+
+else
+break
+
+fi
+done
 
 echo "The partial multiplication table for $num: "
 
@@ -43,17 +56,34 @@ do
     echo "$num x $i = $total"
 done
 
-fi
 
-read -p "Do you want to use the program again? (Enter 'y' for yes, 'n' for no): " ans1
 
-if [ $ans1 != "y" | $ans1 != "n" ]; then
+elif [ $ans != "p" ] && [ $ans != "f" ]; then
 
 continue
 
+fi
+
+
+#######################################################################################################
+while true
+do
+
+read -p "Do you want to use the program again? (Enter 'y' for yes, 'n' for no): " ans1
+if [ $ans1 = "y" ]; then
+break
+
 else
+continue
+fi
+done
+
+if [ $ans1 = "n" ]; then
 echo "Thank you for using the program"
 break
 fi
+#######################################################################################################
+
+
 
 done
